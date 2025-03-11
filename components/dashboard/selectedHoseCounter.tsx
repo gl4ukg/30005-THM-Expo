af8a4731ea2343dbbaaaba4ece83ddd97eb627f5
Hjalti Hjaltason <hjaltihjaltason@NO-C02ZMYJMMD6N.local>
2025-03-11 15:17:18 +0100

    redesign implemented

--- components/dashboard/selectedHoseCounter.tsx
+++ components/dashboard/selectedHoseCounter.tsx
@@ -1,55 +1,40 @@
-import { View, StyleSheet, Pressable } from "react-native";
-import { IconName } from "../Icon/iconMapping";
-import { Icon } from "../Icon/Icon";
-import { Typography } from "../typography";
-import { colors } from "@/lib/tokens/colors";
+import { View, StyleSheet, Pressable } from 'react-native';
+import { IconName } from '../Icon/iconMapping';
+import { Icon } from '../Icon/Icon';
+import { Typography } from '../typography';
+import { colors } from '@/lib/tokens/colors';
 
 interface Props {
-    icon:IconName,
-    counter:number,
-    handlePress: () => void,
+  icon: IconName;
+  counter: number;
+  handlePress: () => void;
 }
 
 export const SelectedHoseCounter: React.FC<Props> = ({
-    icon,
-    counter,
-    handlePress,
+  icon,
+  counter,
+  handlePress,
 }) => {
-
-    return(
-        <Pressable onPress={handlePress}>
-        <View style={elementStyle.frame}>
-            <View style={elementStyle.iconWrapper}>
-                <Icon name={icon} color={colors.primary} size="md"/>
-            </View>
-            <View style={elementStyle.circle}>
-                <Typography name={"navigation"} text={counter.toString()}style={{color:colors.white, margin:"auto"}}/>
-            </View>
-        </View>
-        </Pressable>
-    )
-
+  return (
+    <Pressable onPress={handlePress}>
+      <View style={elementStyle.frame}>
+        <Typography
+          name={'navigation'}
+          text={'(' + counter.toString() + ')'}
+          style={elementStyle.counter}
+        />
+        <Icon name={icon} color={colors.primary} size='md' />
+      </View>
+    </Pressable>
+  );
 };
 
 const elementStyle = StyleSheet.create({
-    frame:{
-        width:40,
-        height:40,
-    },
-    circle: {
-        position: "absolute",
-        top: 2,
-        right: 3,
-        width: 22,
-        height: 22,
-        borderRadius: 11, 
-        backgroundColor: colors.secondary25,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    iconWrapper:{
-        position:"absolute",
-        left:3,
-        bottom:0,
-    }
-})
+  frame: {
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  counter: { color: colors.secondary25, textAlign: 'right', paddingLeft: 2 },
+});
