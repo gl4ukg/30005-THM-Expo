2e8a698e3978d9828a4fc86d67d53d7e606ddf06
Marcin Sawczuk <marcin.sawczuk@itera.com>
2025-03-12 13:06:43 +0100

    hardcoded  user

--- context/THSInitialState.ts
+++ context/THSInitialState.ts
@@ -1,23 +1,27 @@
 export const THSInitialState = {
-    user: null,
-    units: {
-        availableUnits: [],
-        chosenUnitId: null
-    }
-} 
+  user: {
+    email: 'user@e.mail',
+    name: 'user',
+    id: 'id',
+  },
+  units: {
+    availableUnits: [],
+    chosenUnitId: null,
+  },
+};
 
 export type THSStateType = {
-    user: null | {
-        email: string,
-        name: string, 
-        id: string, 
-    },
-    units: {
-        availableUnits: {
-            name: string, 
-            id: string,
-            hoses: any[]
-        }[],
-        chosenUnitId: string | null
-    }
-}
+  user: null | {
+    email: string;
+    name: string;
+    id: string;
+  };
+  units: {
+    availableUnits: {
+      name: string;
+      id: string;
+      hoses: any[];
+    }[];
+    chosenUnitId: string | null;
+  };
+};
