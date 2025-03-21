dcafd1c33c393a183c53a056475e265ec4135b6f
hjalti.hjaltason <hjalti.hjaltason@itera.com>
2025-03-21 11:03:51 +0000

    Merged PR 78: datepicker implemented and added in UI page
    Related work items: #1712

---
+++ components/UI/Input/DateInput.tsx
@@ -0,0 +1,75 @@
+import { View, StyleSheet, Pressable } from 'react-native';
+import { Typography } from '../../typography';
+import { colors } from '@/lib/tokens/colors';
+import { Icon } from '../../Icon/Icon';
+import { useState } from 'react';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+
+interface datePickerProps {
+  label: string;
+}
+
+export const DateInput: React.FC<datePickerProps> = ({ label }) => {
+  const [date, setDate] = useState<Date | null>(null);
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const confirm = (date: Date) => {
+    setDate(date);
+    hideDatePicker();
+  };
+  return (
+    <View>
+      <Typography name={'navigation'} style={styles.label} text={label} />
+      <Pressable style={styles.inputContainer} onPress={showDatePicker}>
+        <Typography
+          name='navigation'
+          text={date ? String(date.toLocaleDateString()) : 'Select date'}
+          style={styles.valueStyle}
+        />
+        <View style={styles.iconContainer}>
+          <Icon name='Calendar' size='md' color={colors.extended666} />
+        </View>
+      </Pressable>
+      <DateTimePickerModal
+        date={date ?? undefined}
+        isVisible={isDatePickerVisible}
+        mode='date'
+        display='inline'
+        onConfirm={confirm}
+        onCancel={hideDatePicker}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  label: {
+    marginBottom: 5,
+    color: colors.extended666,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 5,
+    paddingHorizontal: 12,
+    borderWidth: 1,
+    borderColor: colors.secondary95,
+    borderRadius: 1,
+    backgroundColor: colors.white,
+    width: '100%',
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  valueStyle: { color: colors.extended333 },
+});
