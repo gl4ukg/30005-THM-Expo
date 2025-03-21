5645db3b0a84816102708708f2c71248989cc649
Eivind Guren <eivindguren@gmail.com>
2025-03-21 12:07:42 +0100

    fix: add maintanance info

--- components/detailView/MaintananceInfo.tsx
+++ components/detailView/MaintananceInfo.tsx
@@ -10,6 +10,7 @@ import { RadioGroup } from '../detailHose/radioGroup';
 import { HID } from './types';
 import { Input } from '../UI/Input/input';
 import { TooltipWrapper } from '../detailHose/tooltipWrapper';
+import EditMaintananceInfo from './edit/EditMaintananceInfo';
 
 type MaintananceProps = {
   hoseData: HID;
@@ -21,11 +22,7 @@ const MaintananceInfo: React.FC<MaintananceProps> = ({
   editMode,
   onInputChange,
 }) => {
-  const handleApprovalChange = (selectedLabel: string) => {
-    onInputChange('approved', selectedLabel);
-  };
   const today = new Date();
-
   const nextInspectionError = stringToDate(hoseData.nextInspection) < today;
 
   return (
@@ -42,88 +39,25 @@ const MaintananceInfo: React.FC<MaintananceProps> = ({
       />
 
       {editMode ? (
-        <>
-          <TooltipWrapper>
-            <Input
-              label='Inspected By:'
-              value={hoseData.inspectedBy}
-              onChangeText={(text) => onInputChange('inspectedBy', text)}
-              type={'text'}
-            />
-          </TooltipWrapper>
-          <TooltipWrapper tooltipData={{ title: 'Condition', message: '' }}>
-            <SelectField
-              label='Condition:'
-              value={hoseData.hoseCondition}
-              onChange={(value) => onInputChange('hoseCondition', value)}
-              options={[]}
-            />
-          </TooltipWrapper>
-          <TooltipWrapper>
-            <RadioGroup
-              label={'Approved:'}
-              choices={[
-                { id: 'Yes', label: 'YES' },
-                { id: 'No', label: 'NO' },
-                { id: 'NotInsepcted', label: 'Not inspected' },
-              ]}
-              selected={hoseData.approved}
-              onChange={handleApprovalChange}
-              type={'horizontal'}
-            />
-          </TooltipWrapper>
-          <TooltipWrapper>
-            <Input
-              label={'Comment:'}
-              value={hoseData.comment}
-              onChangeText={(text) => onInputChange('comment', text)}
-              type='textArea'
-            />
-          </TooltipWrapper>
-
-          <Typography
-            name={'navigationBold'}
-            text='Criticality / Intervals'
-            style={styles.subTitle}
-          />
-          <TooltipWrapper>
-            <SelectField
-              label='Criticality'
-              value={hoseData.criticality}
-              onChange={(value) => onInputChange('criticality', value)}
-              options={[
-                { id: 'Not set', label: 'Not set' },
-                { id: '1 - None', label: '1 - None' },
-                { id: '2 - Very low', label: '2 - Very low' },
-                { id: '3 - Low', label: '3 - Low' },
-                { id: '4 - Medium', label: '4 - Medium' },
-                { id: '5 - High', label: '5 - High' },
-                { id: '6 - Very high', label: '6 - Very high' },
-              ]}
-            />
-          </TooltipWrapper>
-        </>
+        <EditMaintananceInfo
+          hoseData={hoseData}
+          onInputChange={onInputChange}
+        />
       ) : (
         <>
           <Datafield label={'Inspected By:'} value={hoseData.inspectedBy} />
-
           <Datafield label={'Condition:'} value={hoseData.hoseCondition} />
-
           <Datafield label={'Approved:'} value={hoseData.approved} />
-
           <Datafield label={'Comment:'} value={hoseData.comment} />
-
           <Typography
             name={'navigationBold'}
             text='Criticality / Intervals'
             style={styles.subTitle}
           />
-
           <Datafield
             label={'Hose Production Date:'}
             value={formatDate(hoseData.prodDate)}
           />
-
           <Datafield label={'Criticality:'} value={hoseData.criticality} />
         </>
       )}
@@ -132,18 +66,15 @@ const MaintananceInfo: React.FC<MaintananceProps> = ({
           label={'Inspection Interval:'}
           value={hoseData.inspectionInterval}
         />
-
         <Datafield
           label={'Next Inspection:'}
           value={formatDate(hoseData.nextInspection)}
           error={nextInspectionError}
         />
-
         <Datafield
           label={'Replacement Interval:'}
           value={hoseData.replacementInterval}
         />
-
         <Datafield
           label={'Replacement Date:'}
           value={formatDate(hoseData.replacementDate)}
